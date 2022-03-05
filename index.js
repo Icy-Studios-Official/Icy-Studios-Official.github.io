e3228237eb01506d86363e72655839d4a795e1fb
TheInkDevil <82723919+TheInkDevil@users.noreply.github.com>
2022-03-04 22:00:12 -0600

    a
    a

---
+++ index.js
@@ -0,0 +1,10 @@
+// no secrets here =)
+var yes = document.getElementById("funni");
+var quotes = [
+    "Welcome to our website, where nothing happens",
+    "Why did you come to this website? There's nothing here!",
+    "Did you find this site on Google? I'll need to fix that if so",
+    "Out of all the websites on the internet, you visit this one."
+];
+yes.innerText = quotes[Math.floor(Math.random() * quotes.length)];
+//# sourceMappingURL=index.js.map
